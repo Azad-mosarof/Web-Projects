fec6e937ff304cb054d3ecca876f297b92cc1576
Azad Mosarof <98151626+Azad-mosarof@users.noreply.github.com>
2022-12-16 19:05:59 +0300

    Add files via upload

---
+++ Draw_Lottery_and_win/draw.js
@@ -0,0 +1,38 @@
+
+const g_btt=document.getElementById('g_btt');
+const lott_no=document.getElementById('lott_no');
+
+window.addEventListener('load',()=>{
+    const myImage=sessionStorage.getItem("myImage");
+    document.getElementById('lott-image').setAttribute('src',myImage);
+    var rest_value=0;
+    sessionStorage.setItem("myImage",rest_value);
+    console.log(myImage);
+});
+
+
+g_btt.addEventListener('click',function(){
+    const lott=random()
+    lott_no.innerHTML=lott
+});
+
+function random(){
+    let rnd_no=[]
+    const alphabet=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','U','V','S']
+    for(let i=0;i<12;i++){
+
+        if(i==2 || i==5 || i==8 || i==11){
+            let x=Math.floor(Math.random()*20);
+            rnd_no.push(alphabet[x]);
+        }
+        else{
+            let y=Math.floor(Math.random()*10);
+            rnd_no.push(y);
+        }
+        
+    }
+    let g_no=rnd_no.join('');
+    return g_no;
+}
+
+
